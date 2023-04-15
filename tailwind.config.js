eb65b2d8d19a8cc6ef4b22c77f15ca4f91fef973
Daníel Adams <me@dandabs.io>
2023-04-15 21:33:00 +0100

    Updated banner image

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	theme: {
 		extend: {
 			backgroundImage: {
-				hero: "linear-gradient(rgba(0,0,0,0.85), rgba(0.1,0,0,0.85)), linear-gradient(black, black), url('/images/backdrop.png'), url('https://www.thoughtco.com/thmb/PndRXg4t6XDJ2wLqMhTSlPq5KHk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/GettyImages-755651077-5b3fedf646e0fb005bc0269e.jpg')",
+				hero: "linear-gradient(rgba(0,0,0,0.85), rgba(0.1,0,0,0.85)), linear-gradient(black, black), url('/images/backdrop.png'), url('/images/DSC01874.jpeg')",
 			},
 			colors: {
 				"ic-blue": "#00E8FF",
