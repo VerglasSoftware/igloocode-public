9ca321643d012b3973fa1f347efc6ce33ae4ade2
Daníel Adams <me@dandabs.io>
2023-04-15 20:49:22 +0100

    Removed dashboard

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,7 @@
-import { ToastContainer } from "react-toastify";
-import { UserProvider } from '@auth0/nextjs-auth0/client';
-import "react-toastify/dist/ReactToastify.css";
 import "../public/styles/global.css";
 
 export default function MyApp({ Component, pageProps: { ...pageProps } }) {
 	return (
-		<UserProvider>
 			<Component {...pageProps} />
-			<ToastContainer position="bottom-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="light" />
-		</UserProvider>
 	);
 }
