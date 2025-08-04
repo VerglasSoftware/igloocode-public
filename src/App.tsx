fb24ac80a32d1e77ebdee668635569a7ec61dfaa
DylanMckenzie <dylan20061@outlook.com>
2025-08-05 00:41:01 +0100

    routin

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { HelmetProvider } from 'react-helmet-async'
+import { Routes, Route } from "react-router-dom";
 
-import ScrollToTop from './components/ScrollToTop'
-import Home from './pages/Home'
-import OurStory from './pages/OurStory'
-import Competition from './pages/Competition'
-import GetInvolved from './pages/GetInvolved'
-import Perks from './pages/Perks'
-import SixthForm from './pages/SixthForm'
-import Teachers from './pages/Teachers'
-import Volunteer from './pages/Volunteer'
+import ScrollToTop from "./lib/ScrollToTop";
+import Home from "./pages/Home";
+import OurStory from "./pages/OurStory";
+import Competition from "./pages/Competition";
+import GetInvolved from "./pages/GetInvolved";
+import Perks from "./pages/Perks";
+import SixthForm from "./pages/SixthForm";
+import Teachers from "./pages/Teachers";
+import Volunteer from "./pages/Volunteer";
 
 function App(): JSX.Element {
   return (
-    <HelmetProvider>
-      <Router>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/ourstory" element={<OurStory />} />
-          <Route path="/competition" element={<Competition />} />
-          <Route path="/getinvolved" element={<GetInvolved />} />
-          <Route path="/getinvolved/sixthform" element={<SixthForm />} />
-          <Route path="/getinvolved/teachers" element={<Teachers />} />
-          <Route path="/perks" element={<Perks />} />
-          <Route path="/volunteer" element={<Volunteer />} />
-        </Routes>
-      </Router>
-    </HelmetProvider>
-  )
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/ourstory" element={<OurStory />} />
+        <Route path="/competition" element={<Competition />} />
+        <Route path="/getinvolved" element={<GetInvolved />} />
+        <Route path="/getinvolved/sixthform" element={<SixthForm />} />
+        <Route path="/getinvolved/teachers" element={<Teachers />} />
+        <Route path="/perks" element={<Perks />} />
+        <Route path="/volunteer" element={<Volunteer />} />
+      </Routes>
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <HelmetProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </HelmetProvider>
+  </React.StrictMode>
+);
