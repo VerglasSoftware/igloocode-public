b14d3656661e28d1f022db6e087ddf5dc40ee581
Daníel Adams <me@dandabs.io>
2026-03-16 16:37:11 +0000

    play redirect

---
+++ pages/play.js
@@ -0,0 +1,13 @@
+export async function getServerSideProps() {
+	return {
+		redirect: {
+			destination: "https://educa.tr/play/qm8b8ioee8sroi2p2qkpfuzp",
+			permanent: false,
+		},
+	};
+}
+
+export default function Play() {
+	return null;
+}
+
